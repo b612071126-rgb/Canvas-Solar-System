9e91b35b49df12285b9b2bbbc656f56fe2c304f9
b612071126-rgb <b612071126@gmail.com>
2026-07-24 10:44:24 +0800

    更新 app.js

--- app.js
+++ app.js
@@ -16,3 +16,18 @@ window.innerHeight;
 
 console.log(canvas.width);
 console.log(canvas.height);
+
+
+ctx.fillStyle = "yellow";
+
+ctx.beginPath();
+
+ctx.arc(
+    canvas.width / 2,
+    canvas.height / 2,
+    50,
+    0,
+    Math.PI * 2
+);
+
+ctx.fill();
